3cc388de4d7523003d043fb075c06f197ef9018b
Mongkon38 <buhtpeng@gmail.com>
2021-06-29 03:15:20 +0700

    bser

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Home from './Pages';
+import { BrowserRouter } from "react-router-dom";
+
 
 function App() {
   return (
     <>
-      <Home />
+      <BrowserRouter basename={'/resume'}>
+        <Home />
+      </BrowserRouter>
     </>
   );
 }
